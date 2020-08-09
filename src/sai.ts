a608679dbe759526bac694d1f222cb6f551ad93e
Jaeiya <exanix@gmail.com>
2020-08-09 16:14:41 -0700

    Add addWord() and delWord() to SAI{}
    These methods are still a work in progress, as they need to actually save the data they're updating, to a file.

--- src/sai.ts
+++ src/sai.ts
@@ -16,6 +16,12 @@ export default class SAI {
   #dictPath   : string;
   #replies    : Replies;
   #words      : string[][];
+  #wordsRef!  : string[];
+
+
+  get words(): string[][] {
+    return this.#words.slice();
+  }
 
 
   constructor(dataFolderPath: string) {
@@ -27,15 +33,71 @@ export default class SAI {
     createGzipFile(this.#repliesPath, replySchema.toBuffer({ replies: [] }));
     createGzipFile(this.#dictPath, dictSchema.toBuffer({ words: [] }));
 
-    this.#replies = readReplyStore(this.#repliesPath, replySchema);
-    this.#words   = readDictStore(this.#dictPath, dictSchema);
+    this.#replies  = readReplyStore(this.#repliesPath, replySchema);
+    this.#words    = readDictStore(this.#dictPath, dictSchema);
+
+    this.updateWordsRef();
 
     console.log(this.#replies);
     console.log(this.#words);
   }
 
 
+  addWord(word: string, index?: number): true | Error {
+    if (this.dictHasWord(word)) return new Error('Word already exists.');
+
+    if (typeof index != 'number') {
+      this.#words.push([word]);
+    }
+
+    if (typeof index == 'number') {
+      if (index < 0)           return new Error('Index must be greater than -1.');
+      if (!this.#words[index]) return new Error(`The index "${index}" does not exist.`);
+      this.#words[index].push(word);
+    }
+
+    this.updateWordsRef();
+    return true;
+  }
+
+
+  delWord(word: string, index: number): boolean | Error {
+    if (index < 0) return new Error('Index must be greater than -1.');
+
+    if (!this.#words[index]) {
+      return Error(`The index "${index}" does not exist.`);
+    }
+
+    const wordIndex = this.#words[index].indexOf(word);
+    if (!~wordIndex) {
+      return new Error(`The word "${word}" does not exist at index "${index}".`);
+    }
+
+    this.#words[index].splice(wordIndex, 1);
+    // Delete entire array if it's empty
+    if (!this.#words[index].length) {
+      this.#words.splice(index, 1);
+    }
+    this.updateWordsRef();
+    return true;
+  }
+
+
+  private dictHasWord(word: string): boolean {
+    if (!this.#wordsRef.length) return false;
+    if (~this.#wordsRef.indexOf(word)) return true;
+    return false;
+  }
+
+
+  private updateWordsRef() {
+    this.#wordsRef = _flatten(this.#words);
+  }
+
+
 }
+
+
 
 
 
