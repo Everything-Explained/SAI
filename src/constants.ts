4ce433211ec15c340fd2a3e57003c9d0a6082f1e
Jaeiya <exanix@gmail.com>
2020-08-10 19:17:55 -0700

    Add constants file along with tests

---
+++ src/constants.ts
@@ -0,0 +1,43 @@
+export const queryTokens = [
+    'how', 'has', 'what', 'when',
+  'where', 'why',  'who', 'does',
+    'can', 'are', 'tell',   'is',
+     'do', 'did', 'will',
+];
+
+
+export const contextTokens = [
+  'this',   'was',  'like',     'i',
+    'of',  'much',  'have',   'and',
+    'in',   'you',    'me',  'your',
+  'from',    'we', 'about',  'know',
+   'how',   'not',  'whom',    'us',
+  'what',    'am',    'be',  'when',
+  'them',    'to',   'has', 'there',
+   'our', 'those',  'here',
+];
+
+
+export const optionalTokens = [
+  'a',    'an',    'do',   'can', 'the',
+  'are',  'does',  'it',   'is',
+  'that', 'could', 'would',
+];
+
+
+export const contractionCorrections = [
+  [/haven't/g, 'have not'],
+  [/aren't/g,  'are not'],
+  [/won't/g,   'will not'],
+  [/mustn't/g, 'must not'],
+  [/can't/g,   'can not'],
+  [/n't/g,     ' not'],
+  [/'ll/g,     ' will'],
+  [/'s/g,      ' is']
+];
+
+
+export const matchInvalidChars = /[^a-zA-Z]/g;
+
+
+export const hashSeed = 0xAE10;
---
+++ test/constants.test.ts
@@ -0,0 +1,106 @@
+import { contextTokens, hashSeed, optionalTokens, queryTokens } from "../src/constants";
+import { intersection as _intersection } from 'lodash/fp';
+import del from 'del';
+import t from 'tape';
+
+
+
+
+function tokensAreUnique(tokens: string[], tokenType: string) {
+  const clonedTokens = tokens.slice();
+  while (clonedTokens.length) {
+    const token = clonedTokens.shift() as string;
+    if (~clonedTokens.indexOf(token)) {
+      throw new Error(`Duplicate ${tokenType} token found: "${token}"`);
+    }
+  }
+  return true;
+}
+
+
+function tokensHaveNoSpaces(tokens: string[], tokenType: string) {
+  tokens.forEach(v => {
+    if (v.includes(' '))
+      throw new Error(`Whitespace found within ${tokenType} token: "${v}"`)
+    ;
+  });
+  return true;
+}
+
+t('Constants', async t => {
+
+  t.test('[test setup]', async t => {
+    let tokens = ['1', '2', '3', '4', '5'];
+    t.ok(
+      tokensAreUnique(tokens, 'validation'),
+      'tokensAreUnique(): returns true on no duplicate tokens.'
+    );
+
+    tokens = ['1', '2', '3', '4', '5', '3'];
+    t.throws(
+      () => tokensAreUnique(tokens, 'invalidation'),
+      'tokensAreUnique(): throws an error on duplicate tokens'
+    );
+
+    tokens = ['1', '2', '3', '4', '5'];
+    t.ok(
+      tokensHaveNoSpaces(tokens, 'NoSpaces'),
+      'tokensHaveNoSpaces(): enforces no space in tokens.'
+    );
+
+    tokens = ['1', '2', '3 ', '4', '5'];
+    t.throws(
+      () => tokensHaveNoSpaces(tokens, 'HaveSpaces'),
+      'tokensHaveNoSpaces(): throws when spaces found in tokens.'
+    );
+  });
+
+
+  t.test('hashSeed', async t => {
+    t.equal(hashSeed, 0xAE10, 'is 0xAE10.');
+  });
+
+
+  t.test('queryTokens', async t => {
+    t.ok(
+      tokensAreUnique(queryTokens, 'query'),
+      'are unique.'
+    );
+
+    t.ok(
+      tokensHaveNoSpaces(queryTokens, 'query'),
+      'contain no spaces.'
+    );
+  });
+
+
+  t.test('contextTokens', async t => {
+    t.ok(
+      tokensAreUnique(contextTokens, 'context'),
+      'are unique.'
+    );
+    t.ok(
+      tokensHaveNoSpaces(contextTokens, 'context'),
+      'contain no spaces.'
+    );
+  });
+
+
+  t.test('optionalTokens', async t => {
+    t.ok(
+      tokensAreUnique(optionalTokens, 'optional'),
+      'are unique.'
+    );
+
+    t.ok(
+      tokensHaveNoSpaces(optionalTokens, 'optional'),
+      'contain no spaces.'
+    );
+
+    const diff = _intersection(optionalTokens, contextTokens);
+    t.equal(
+      diff.length, 0,
+      'do not intersect with contextual tokens.'
+    );
+  });
+});
