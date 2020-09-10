11ff0d2d140c23d43cbf993cd9ec42620d816d26
Jaeiya <exanix@gmail.com>
2020-09-10 14:51:30 -0700

    Remove dist directory (oops)

--- dist/variables/constants.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const queryTokens: string[];
-export declare const contextTokens: string[];
-export declare const optionalTokens: string[];
-export declare const contractionCorrections: [RegExp, string][];
-export declare const matchInvalidChars: RegExp;
-export declare const hashSeed = 1345;
-export declare const testDir = "./test/mocks";
