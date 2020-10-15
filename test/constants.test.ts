566e079b6fd4b400c39c50fe544f44b99dcfc268
Jaeiya <exanix@gmail.com>
2020-10-14 17:54:12 -0700

    Update Constants tests to use new structure

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -29,75 +29,57 @@ function tokensHaveNoSpaces(tokens: string[], tokenType: string) {
 }
 
 t('Constants', async t => {
-  t.test('[test setup]', async t => {
-    let tokens = ['1', '2', '3', '4', '5'];
-    t.ok(
-      tokensAreUnique(tokens, 'validation'),
-      'tokensAreUnique(): returns true on no duplicate tokens.'
-    );
 
-    tokens = ['1', '2', '3', '4', '5', '3'];
-    t.throws(
-      () => tokensAreUnique(tokens, 'invalidation'),
-      'tokensAreUnique(): throws an error on duplicate tokens'
-    );
-
-    tokens = ['1', '2', '3', '4', '5'];
-    t.ok(
-      tokensHaveNoSpaces(tokens, 'NoSpaces'),
-      'tokensHaveNoSpaces(): enforces no space in tokens.'
-    );
-
-    tokens = ['1', '2', '3 ', '4', '5'];
-    t.throws(
-      () => tokensHaveNoSpaces(tokens, 'HaveSpaces'),
-      'tokensHaveNoSpaces(): throws when spaces found in tokens.'
-    );
+  t.test('[setup]:tokensAreUnique() returns true when there are NO duplicate tokens.', async t => {
+    const tokens = ['1', '2', '3', '4', '5'];
+    t.ok(tokensAreUnique(tokens, 'validation'));
   });
 
-
-  t.test('queryTokens', async t => {
-    t.ok(
-      tokensAreUnique(queryTokens, 'query'),
-      'are unique.'
-    );
-    t.ok(
-      tokensHaveNoSpaces(queryTokens, 'query'),
-      'contain no spaces.'
-    );
-    t.ok(queryTokens.length < 26,
-      'should have less than 26 items.'
-    );
+  t.test('[setup]:tokensAreUnique() throws an error when duplicate tokens are found.', async t => {
+    const tokens = ['1', '2', '3', '4', '5', '3'];
+    t.throws(() => tokensAreUnique(tokens, 'invalidation'));
   });
 
-
-  t.test('contextTokens', async t => {
-    t.ok(
-      tokensAreUnique(contextTokens, 'context'),
-      'are unique.'
-    );
-    t.ok(
-      tokensHaveNoSpaces(contextTokens, 'context'),
-      'contain no spaces.'
-    );
+  t.test('[setup]:tokensHaveNoSpaces() returns true when there are NO spaces between tokens.', async t => {
+    const tokens = ['1', '2', '3', '4', '5'];
+    t.ok(tokensHaveNoSpaces(tokens, 'NoSpaces'));
   });
 
+  t.test('[setup]:tokensHaveNoSpaces() throws an error when spaces are found in tokens.', async t => {
+    const tokens = ['1', '2', '3 ', '4', '5'];
+    t.throws(() => tokensHaveNoSpaces(tokens, 'HaveSpaces'));
+  });
 
-  t.test('optionalTokens', async t => {
-    t.ok(
-      tokensAreUnique(optionalTokens, 'optional'),
-      'are unique.'
-    );
+  t.test('queryTokens should be unique', async t => {
+    t.ok(tokensAreUnique(queryTokens, 'query'));
+  });
 
-    t.ok(
-      tokensHaveNoSpaces(optionalTokens, 'optional'),
-      'contain no spaces.'
-    );
+  t.test('queryTokens should contain no spaces', async t => {
+    t.ok(tokensHaveNoSpaces(queryTokens, 'query'));
+  });
 
+  t.test('queryTokens should have less than 26 items.', async t => {
+    t.ok(queryTokens.length < 26);
+  });
+
+  t.test('contextTokens should be unique.', async t => {
+    t.ok(tokensAreUnique(contextTokens, 'context'));
+  });
+
+  t.test('contextTokens should contain no spaces.', async t => {
+    t.ok(tokensHaveNoSpaces(contextTokens, 'context'));
+  });
+
+  t.test('optionalTokens should be unique.', async t => {
+    t.ok(tokensAreUnique(optionalTokens, 'optional'));
+  });
+
+  t.test('optionalTokens should contain no spaces.', async t => {
+    t.ok(tokensHaveNoSpaces(optionalTokens, 'optional'));
+  });
+
+  t.test('optionalTokens should not intersect with contextual tokens.', async t => {
     const diff = _intersection(optionalTokens, contextTokens);
-    t.equal(
-      diff.length, 0,
-      'do not intersect with contextual tokens.'
-    );
+    t.equal(diff.length, 0);
   });
 });
