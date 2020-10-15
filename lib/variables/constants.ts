e00fb27eb36ae006e82cb7cbbe49a790e528751b
Jaeiya <exanix@gmail.com>
2020-10-14 23:35:49 -0700

    Make constants `readonly`

--- lib/variables/constants.ts
+++ lib/variables/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  static queryTokens = [
+  static readonly queryTokens = [
       'how',  'has', 'what', 'when',
     'where',  'why',  'who', 'does',
       'can',  'are',   'is',   'do',
@@ -7,7 +7,7 @@ export class Constants {
   ];
 
 
-  static contextTokens = [
+  static readonly contextTokens = [
     'this',   'was',  'like',     'i',
       'of',  'have',   'and',  'here',
       'in',   'you',    'me',  'your',
@@ -19,14 +19,14 @@ export class Constants {
   ];
 
 
-  static optionalTokens = [
+  static readonly optionalTokens = [
     'a',    'an',    'do',   'can', 'the',
     'are',  'does',  'it',   'is',
     'that', 'could', 'would', 'did'
   ];
 
 
-  static contractionMatrix: [RegExp, string][] = [
+  static readonly contractionMatrix: [RegExp, string][] = [
     [/won't/g        , 'will not'],
     [/can't|cannot/g , 'can not'],
     [/n't/g          , ' not'],
@@ -36,7 +36,7 @@ export class Constants {
   ];
 
 
-  static matchInvalidChars = /[^a-z]/g;
-  static mockDir = './test/mocks';
+  static readonly matchInvalidChars = /[^a-z]/g;
+  static readonly mockDir = './test/mocks';
 }
 
