d166a8edb37ded1d10415e2a1dc61b28a6546dbb
Jaeiya <exanix@gmail.com>
2020-10-15 17:21:55 -0700

    Fix mock folder in FileOps{} tests

--- test/file-ops.test.ts
+++ test/file-ops.test.ts
@@ -43,46 +43,42 @@ const getRndNum = (range: number) => {
   return Math.floor(Math.random() * range);
 };
 
-const genRandomFilePath = (folderPath: string) => {
-  fileOps.createFolder(folderPath);
-  return `${folderPath}/test${getRndNum(10000)}.gzip`;
+const mockFolder = `${Constants.mockDir}/file-ops`;
+
+const genRandomFilePath = () => {
+  fileOps.createFolder(`${mockFolder}`);
+  return `${mockFolder}/test${getRndNum(10000)}.gzip`;
 };
 
 
 t('File Operations', async t => {
 
   t.test('createFolder() returns true on successful folder creation.', async t => {
-    const path = `${Constants.mockDir}/createFolder`;
+    const path = `${mockFolder}`;
     fileOps.createFolder(path);
     t.ok(existsSync(path));
-    del(path);
   });
 
   t.test('createFolder() returns true if folder already exists.', async t => {
-    const path = `${Constants.mockDir}/duplicateFolderTest`;
+    const path = `${mockFolder}`;
     fileOps.createFolder(path);
     t.ok(fileOps.createFolder(path));
-    del(path);
   });
 
   t.test('save() returns promised null on success.', t => {
     t.plan(1);
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     fileOps.save(path, testScheme, ['valid', 'value'], false)
-    .then(data => {
-      t.equal(data, null);
-      del(path);
-    });
+    .then(data => t.equal(data, null));
   });
 
   t.test('save() returns promise rejection when saving too fast.', t => {
     t.plan(1);
-    const saveFirstPath = genRandomFilePath(Constants.mockDir);
+    const saveFirstPath = genRandomFilePath();
     fileOps
       .save(saveFirstPath, testScheme, ['valid', 'value'], false)
-      .then(() => del(saveFirstPath)) // cleanup
     ;
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     fileOps.save(path, testScheme, ['valid value'], false)
       .then(() => t.fail('Saved during another save operation (whoops).'))
       .catch((err: Error) => t.ok(~err.message.indexOf('Cannot save')))
@@ -91,7 +87,7 @@ t('File Operations', async t => {
 
   t.test('save() returns promise rejection on invalid scheme value.', t => {
     t.plan(1);
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     fileOps
       .save(path, testScheme, 'invalid value', false, false)
       .catch((err: Error) => t.ok(~err.message.indexOf('data fails')))
@@ -108,46 +104,43 @@ t('File Operations', async t => {
 
   t.test('save() returns promised null on file compression and save success.', t => {
     t.plan(1);
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     fileOps
       .save(path, testScheme, ['valid', 'value'], true, false)
-      .then(resp => {t.equal(resp, null); del(path); })
+      .then(resp => t.equal(resp, null))
     ;
   });
 
   t.test('readInquiryStore() returns an InquiryRecord Array.', async t => {
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     await fileOps.save(path, inquiryScheme, mockInquiryData, true, false);
     const inquiries = fileOps.readInquiryStore(path);
     t.ok(Array.isArray(inquiries));
     t.is(inquiries[0].compare(mockInquiryData[0]), 0);
-    del(path);
   });
 
   t.test('readInquiryStore() throws an error if data fails schema conversion.', async t => {
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     await writeFileAsync(path, JSON.stringify({ hello: ''}), { encoding: 'binary'});
     t.throws(
       () => fileOps.readInquiryStore(path),
       /(truncated buffer)|(incorrect header)/g)
     ;
-    del(path);
   });
 
   t.test('readDictStore() returns a dictionary file into a JSON object..', async t => {
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     await fileOps.save(path, dictSchema, mockDictData, true, false);
     t.same(mockDictData, mockDictData);
-    del(path);
   });
 
   t.test('readDictStore() throws an error if data fails schema conversion.', async t => {
-    const path = genRandomFilePath(Constants.mockDir);
+    const path = genRandomFilePath();
     await writeFileAsync(path, JSON.stringify({ hello: ''}), { encoding: 'binary'});
     t.throws(
       () => fileOps.readDictStore(path),
       /(truncated buffer)|(incorrect header)/g)
     ;
-    del(path);
+    del(mockFolder); // ############# Cleanup for ALL tests #############
   });
 });
