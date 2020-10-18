cfbbf72af8b2c18caee45976fa67359f5bed30fc
Jaeiya <exanix@gmail.com>
2020-10-17 18:47:41 -0700

    Fix Utils.padNumber() to ignore padding beyond magnitude

--- lib/core/utils.ts
+++ lib/core/utils.ts
@@ -8,15 +8,15 @@ export function padStr(str: string, amount: number, char: string) {
 /**
  * Pads a number based on an expected magnitude.
  *
- * @param magnitude A number representing an order of 10. Setting
- *                  magnitude to **1** means you expect a magnitude of
- *                  numbers between **0** and **100**.
+ * @param magnitude The power of 10 you expect to pad a number to.
+ * With a magnitude of: **1** it will only pad `0 through 99`.
  */
 export function padNumber(num: number, magnitude = 0) {
+  if (magnitude < 0)
+    throw Error('Magnitude must be greater than 0')
+  ;
   const places = `${10 * Math.pow(10, magnitude)}`.length;
   const padAmount = places - num.toString().length;
-  if (padAmount < 0)
-    throw Error('Number is greater than specified magnitude.')
-  ;
+  if (padAmount < 0) return num.toString();
   return padStr(num.toString(), padAmount, '0');
 }
